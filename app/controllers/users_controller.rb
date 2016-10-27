483a49b213a3c207e39a4ce96175644317c19f6f
Molly FitzHarris <email": null>
2016-10-27 11:53:59 -0500

    Write tests for succesful creation of user, write necessary code in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      render 'new'
+    end
   end
 
   private
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,4 +12,21 @@ describe UsersController do
       expect(response).to render_template(:new)
     end
   end
+
+  describe "POST #create" do
+    context "when valid params are passed" do
+      it "responds with status code 302" do
+        post :create, { user: { username: "username", email: "email@email.com", password: "password", password_confirmation: "password" }}
+        expect(response).to have_http_status 302
+      end
+
+      it "creates a new user in the database" do
+        expect { post :create, { user: { username: "username", email: "email@email.com", password: "password", password_confirmation: "password" }} }.to change(User, :count).by(1)
+      end
+    end
+
+    context "when invalid params are passed" do
+
+    end
+  end
 end
