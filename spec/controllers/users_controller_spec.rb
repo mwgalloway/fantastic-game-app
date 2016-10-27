ef8957b9915203fc719b1a33e2eb316c5e7c88e6
Molly FitzHarris <email": null>
2016-10-27 11:16:47 -0500

    Write tests for #new in users_controllers_spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe UsersController do
+  describe "GET #new" do
+    it "responds with status code 200" do
+      get :new
+      expect(response).to have_http_status 200
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+end
