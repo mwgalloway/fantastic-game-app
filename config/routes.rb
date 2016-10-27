bd5aacf31511f175adda75aa8721e24efe59c655
John Michael Moore & Laura Moreno & Molly FitzHarris & Matthew Galloway <pair+jmoore314.lauframo.mfitzharris.mwgalloway@devbootcamp.com>
2016-10-27 14:58:16 -0500

    added custom destroy sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  resources :sessions
+  resources :sessions, except: [:destroy]
   resources :games
+
+  delete '/sessions' => "sessions#destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
