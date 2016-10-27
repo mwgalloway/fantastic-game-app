75b76a1e0cd15a0729e95633fd225fd690f09857
John Michael Moore & Laura Moreno & Molly FitzHarris & Matthew Galloway <pair+jmoore314.lauframo.mfitzharris.mwgalloway@devbootcamp.com>
2016-10-27 14:26:15 -0500

    complete test. include helper in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+
   def new
   end
 
   def create
+    @user = User.find_by(username: params[:user])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/games'
+    else
+      @error = "Username and/or password don't match"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
   end
 end
