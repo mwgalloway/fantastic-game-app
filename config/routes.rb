7f1f8c1aa8cce6e88f7ccfc7fe29a31700cb305c
John Michael Moore <johnmoore314@gmail.com>
2016-10-27 17:17:45 -0500

    Add feature to add a game to a users library

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :games
 
   delete '/sessions' => "sessions#destroy"
+
+  resources :libraries, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
