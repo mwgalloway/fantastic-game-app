faa2b350aacef9ee6671b27e42897c5d0a207cdd
John Michael Moore <johnmoore314@gmail.com>
2016-10-27 18:39:15 -0500

    Add functionality to delete a game from your library

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -6,11 +6,14 @@ class LibrariesController < ApplicationController
     user_id = session[:user_id]
     game_id = params[:game_id]
     UserGame.create(user_id: user_id, game_id: game_id)
-    render game_path(game_id)
+    redirect_to game_path(game_id)
   end
 
   def destroy
-
-    puts "YOU ARE TRYING TO DESTROY NOTHING"
+    user_id = session[:user_id]
+    game_id = params[:id]
+    game = UserGame.find_by(user_id: user_id, game_id: game_id)
+    game.destroy
+    redirect_to game_path(game_id)
   end
 end
