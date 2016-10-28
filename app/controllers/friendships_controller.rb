46ef195386c92815145d7ba6cec8250f1a6f0e02
Molly FitzHarris <email": null>
2016-10-28 11:57:27 -0500

    Create view to see new friendship requests

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,11 +2,17 @@ class FriendshipsController < ApplicationController
   include SessionsHelper
   before_action :authenticate
 
+  def index
+    @pending_friends = current_user.pending_friends
+  end
 
   def create
     @user_in_view = User.find_by(id: params[:id])
     @friendship = Friendship.create(user_id: current_user.id, friend_id: params[:id])
 
     redirect_to @user_in_view
+  end
+
+  def update
   end
 end
