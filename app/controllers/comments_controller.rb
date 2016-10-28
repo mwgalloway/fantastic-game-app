f20859f1cf4d5f839433da58710e7378ec09b94e
Matthew Galloway <email": null>
2016-10-28 14:43:41 -0500

    Style error message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,14 @@ class CommentsController < ApplicationController
     @comment.content = params[:comment][:content]
     @comment.user_id = current_user.id
     @comment.game_id = params[:game_id]
-    @comment.save
+    @game = @comment.game
 
-    redirect_to game_path(@comment.game)
+    if @comment.save
+      redirect_to game_path(@comment.game)
+    else
+      flash[:error_object] = @comment
+      render 'games/show'
+    end
   end
 
   def comment_params
